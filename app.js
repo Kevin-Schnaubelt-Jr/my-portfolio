eb900e9f7ae2a0f9647aa6c263deeb828602d228
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-03-31 17:06:06 -0400

    added proper links

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ const app = new Vue({
       {
         id: 1,
         title: 'Linked Merch',
-        description: "Buy something, customize a QR code for it, then design it's path! (In progress)",
+        description: 'Buy something, customize a QR code for it, then design it\'s path! (In progress)',
         image: 'linkmerchsplash.png',
-        link: 'https://www.google.com'
+        link: 'https://github.com/Kevin-Schnaubelt-Jr/Linked-Merch'
       },
       {
         id: 2,
         title: 'Trivia Blaster!',
         description: 'Play a 10 round game of trivia using the Open Trivia Database API! ',
         image: 'trivia blaster.png',
-        link: 'https://www.google.com'
+        link: 'https://github.com/Kevin-Schnaubelt-Jr/Trivia-Blaster'
       },
       {
         id: 3,
         title: 'TCL Financial Aid Calculator',
-        description: 'Created for my colleges financial aid office. Calculates tuition assistance and grants.',
+        description: 'Created for my college\'s financial aid office. Calculates tuition assistance and grants.',
         image: 'FinancialAidCalc port.png',
-        link: 'https://www.google.com'
+        link: 'https://github.com/Kevin-Schnaubelt-Jr/TCL-Financial-Aid-Calculator'
       }
     ]
   },
