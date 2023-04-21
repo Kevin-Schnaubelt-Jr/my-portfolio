03e396470b817213bac01946b2e82c742bd2d0b2
Kevin Schnaubelt Jr <kevinschnaubelt@gmail.com>
2023-04-20 21:39:42 -0400

    changed font and made shading better

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'Kevin Schnaubelt\nSoftware Developer',
+    title: 'KEVIN SCHNAUBELT\nSoftware Developer',
     projects: [
       {
         id: 1,
